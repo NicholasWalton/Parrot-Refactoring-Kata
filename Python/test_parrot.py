cb2a48b59f2965e23e990da4805cafdd22b067d9
Nicholas Walton <nicholas.walton@gmail.com>
2024-06-06 13:50:52 -0700

    Finished?

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -1,56 +1,45 @@
-from parrot import African, European, NorwegianBlue
+from parrot import African, European, Nailed, NorwegianBlue
 
 
 def test_speed_of_european_parrot():
-    parrot = European()
-    assert parrot.speed() == 12.0
+    assert European().speed() == 12.0
 
 
 def test_cry_of_european_parrot():
-    parrot = European()
-    assert parrot.cry() == "Sqoork!"
+    assert European().cry() == "Sqoork!"
 
 
 def test_speed_of_african_parrot_with_one_coconut():
-    parrot = African(1)
-    assert parrot.speed() == 3.0
+    assert African(1).speed() == 3.0
 
 
 def test_cry_of_african_parrot():
-    parrot = African(1)
-    assert parrot.cry() == "Sqaark!"
+    assert African(1).cry() == "Sqaark!"
 
 
 def test_speed_of_african_parrot_with_two_coconuts():
-    parrot = African(2)
-    assert parrot.speed() == 0.0
+    assert African(2).speed() == 0.0
 
 
 def test_speed_of_african_parrot_with_no_coconuts():
-    parrot = African(0)
-    assert parrot.speed() == 12.0
+    assert African(0).speed() == 12.0
 
 
 def test_speed_norwegian_blue_parrot_nailed():
-    parrot = NorwegianBlue(1.5, True)
-    assert parrot.speed() == 0.0
+    assert Nailed(1.5).speed() == 0.0
 
 
 def test_speed_norwegian_blue_parrot_not_nailed():
-    parrot = NorwegianBlue(1.5, False)
-    assert parrot.speed() == 18.0
+    assert NorwegianBlue(1.5).speed() == 18.0
 
 
 def test_speed_norwegian_blue_parrot_not_nailed_high_voltage():
-    parrot = NorwegianBlue(4, False)
-    assert parrot.speed() == 24.0
+    assert NorwegianBlue(4).speed() == 24.0
 
 
 def test_cry_norwegian_blue_parrot_high_voltage():
-    parrot = NorwegianBlue(4, False)
-    assert parrot.cry() == "Bzzzzzz"
+    assert NorwegianBlue(4).cry() == "Bzzzzzz"
 
 
 def test_cry_norwegian_blue_parrot_no_voltage():
-    parrot = NorwegianBlue(0, False)
-    assert parrot.cry() == "..."
+    assert NorwegianBlue(0).cry() == "..."
